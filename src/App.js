d3f4c813c6daef39c66b8b05ea912624ca9f7739
ramandeep5176 <ramandeepgill5176@gmail.com>
2022-02-14 18:41:18 -0500

    products page started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />}>
+          <Home />
+        </Route>
+        <Route exact path="/checkout/:id">
+          <Checkout />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about"></PageHero>
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+            quod animi beatae in incidunt obcaecati laudantium iusto harum!
+            Sequi, mollitia. Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Voluptatibus, aut! Id aspernatur rerum asperiores blanditiis
+            nisi delectus repudiandae eligendi unde?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +53,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
