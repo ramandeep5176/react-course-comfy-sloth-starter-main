b2d0fa430b5d2b4cf3521eadcfd8ef71db66b016
ramandeep5176 <ramandeepgill5176@gmail.com>
2022-02-15 05:29:34 -0500

    add products to page

--- src/components/Services.js
+++ src/components/Services.js
@@ -20,13 +20,11 @@ const Services = () => {
           {services.map((service) => {
             const { id, icon, title, text } = service;
             return (
-              <>
-                <article key={id} className="service">
-                  <span className="icon">{icon}</span>
-                  <h4>{title}</h4>
-                  <p>{text}</p>
-                </article>
-              </>
+              <article key={id} className="service">
+                <span className="icon">{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
             );
           })}
         </div>
